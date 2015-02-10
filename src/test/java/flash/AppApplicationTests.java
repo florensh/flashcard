65f8fd28913acde362660c2cd0eb28e537018834
Florens Hückstädt <fhueckst@stud.hs-heilbronn.de>
2015-02-10 02:00:54 +0100

    secure hello world

---
+++ src/test/java/flash/AppApplicationTests.java
@@ -0,0 +1,18 @@
+package flash;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import de.fh.flashcard.AppApplication;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = AppApplication.class)
+public class AppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
